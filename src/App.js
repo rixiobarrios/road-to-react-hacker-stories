5f6e4216c6d1ca2e6147e3d447c74ad04fdd4b89
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-06-02 14:23:11 -0500

    Embedded JS in JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import * as React from 'react';
 
+function getTitle(title) {
+    return title;
+}
+
+// const title = 'React';
+
 //function component
 function app() {
     return (
-        //not HTML but JSX
+        //not HTML but JSX (Javascript XML syntax)
         <div>
-            <h1>Hello World</h1>
+            <h1>Hello {getTitle('React')}</h1>
+
+            {/* <div>
+            <h1>Hello {title}</h1>
+        </div> */}
+            <label htmlFor="search">Search:</label>
+            <input id="search" type="text" />
         </div>
     );
 }
