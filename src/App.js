0f68012dbea18f3568daccf19723d768440e88b9
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-06-03 09:23:19 -0500

    React Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
 import * as React from 'react';
 
-function getTitle(title) {
-    return title;
-}
+// function getTitle(title) {
+//     return title;
+// }
 
 // const title = 'React';
+
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        point: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
+
+function List() {
+    return (
+        <ul>
+            {list.map(function (item) {
+                return (
+                    <li key={item.objectID}>
+                        <span>
+                            <a href={item.url}>{item.title}</a>
+                            <span>{item.author}</span>
+                            <span>{item.num_comments}</span>
+                            <span>{item.points}</span>
+                        </span>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
+
+function Search() {
+    return (
+        <div>
+            <label htmlFor="search">Search:</label>
+            <input id="search" type="text" />
+        </div>
+    );
+}
 
 //function component
 function app() {
     return (
         //not HTML but JSX (Javascript XML syntax)
         <div>
-            <h1>Hello {getTitle('React')}</h1>
-
+            <h1>My Hacker Stories</h1>
+            {/* <h1>Hello {getTitle('React')}</h1> */}
+            <Search />
             {/* <div>
             <h1>Hello {title}</h1>
         </div> */}
-            <label htmlFor="search">Search:</label>
-            <input id="search" type="text" />
+            <hr />
+            {/* render the list */}
+            <List />
         </div>
     );
 }
