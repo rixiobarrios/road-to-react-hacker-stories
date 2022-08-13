0931530bd266704d65ed623e713a294f1766846a
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-08-13 18:50:17 -0500

    Changed fetch to axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -65,12 +66,13 @@ const App = () => {
         if (!searchTerm) return;
         dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-        fetch(url)
-            .then((response) => response.json())
+        axios
+            .get(url)
+            // .then((response) => response.json())
             .then((result) => {
                 dispatchStories({
                     type: 'STORIES_FETCH_SUCCESS',
-                    payload: result.hits,
+                    payload: result.data.hits,
                 });
             })
             .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
